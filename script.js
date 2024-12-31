4dc2b9e796199df62f600d6bdb7cd4ee1177e132
Daniel Herrera <danielherreraurbano2004@gmail.com>
2024-12-31 00:44:56 +0000

    messaging working

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+emailjs.init('XWVqkjq_nCS1CFnA');
 var currentImageIndex = 1
 const totalImages = 16
+
+function addButtonAnimation() {
+  const instagramButton = document.getElementById('instagram');
+  const behanceButton = document.getElementById('behance');
+  const roverButton = document.getElementById('rover');
+  instagramButton.classList.add('media_icon1');
+  behanceButton.classList.add('media_icon2');
+  roverButton.classList.add('media_icon3');
+}
 
 function changeImageForward() {
   const image = document.getElementById('photo');
@@ -13,24 +23,49 @@ function changeImageBackwards() {
     image.src = `gallery/${currentImageIndex}.jpg`;
 }
 
-emailjs.init('-XWVqkjq_nCS1CFnA');
-
-
-// TODO
-window.onload = function() {
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      emailjs.sendForm('service_6e0yt1h', 'template_f7z5p6s', this)
-          .then(() => {
-              console.log('SUCCESS!');
-              const sentMessage = document.getElementById('sent_message');
-              sentMessage.style.display = 'block';
-              setTimeout(function() {
-                  sentMessage.style.opacity = 1; 
-              }, 10);
-          }, (error) => {
-              console.log('FAILED...', error);
-          });
-  });
+function buttonAnimation(succesful) {
+  const sentMessageButton = document.getElementById('submit_button');
+  succesful ? sentMessageButton.classList.add('submit_button_sent') : sentMessageButton.classList.add('submit_button_not_sent')
 }
 
+function showMessage(messageText) {
+  const messageElement = document.getElementById('message');
+  messageElement.textContent = messageText; 
+  messageElement.style.display = 'block';  
+  messageElement.classList.add('message'); 
+
+  setTimeout(() => {
+    messageElement.style.display = 'none';
+  }, 4000); 
+}
+
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    emailjs.sendForm('service_6e0yt1h', 'template_f7z5p6s', this)
+      .then(() => {
+        this.reset();
+        const inputs = this.querySelectorAll('input, textarea');
+        inputs.forEach(input => input.setAttribute('disabled', 'true'));
+        buttonAnimation(true);
+        showMessage('¡Mensaje enviado!');
+      }, (error) => {
+        buttonAnimation(false);
+        showMessage('El mensaje no ha podido ser enviado');
+      });
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const instagramImage = document.getElementById('instagram');
+  const behanceImage = document.getElementById('behance');
+  const roverImage = document.getElementById('rover');
+  setTimeout(() => {
+      instagramImage.classList.add('media_icon_animation');
+      behanceImage.classList.add('media_icon_animation');
+      roverImage.classList.add('media_icon_animation');
+      instagramImage.classList.remove('media_icon1');
+      behanceImage.classList.remove('media_icon2');
+      roverImage.classList.remove('media_icon3');
+    }, 2000);
+  });
